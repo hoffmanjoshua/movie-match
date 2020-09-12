e85ef7d5f548b61d1442bcff2acb5338ae9c5208
Joshua Hoffman <hoffmanjoshuad@gmail.com>
2020-09-12 02:27:52 -0500

    landing/login page ✅

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.scss";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div class="home-container">
+        <h1>Movie Match</h1>
+        <div className="login-box">
+          <form className="login-form">
+            <label for="Email">Email</label>
+            <input type="text" className="login-email" name="Email" />
+            <label for="Password">Password</label>
+            <input type="password" className="login-password" name="Password" />
+            <input type="submit" className="login-submit" value="Login" />
+          </form>
+          <div class="no-account-prompt">
+            <p>Don't have account?</p>
+            <button className="signup-btn">Create an Account</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
